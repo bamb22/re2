687cd17128cef5d11a87593e9125a7418a673d13
bamb22 <92354176+bamb22@users.noreply.github.com>
2021-12-10 10:13:34 +0900

    20211210
    20211210

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,13 @@ $('.slider').slick({
     autoplaySpeed: 2000,
   });
 
+  $('.slider05').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
 
 ;(function(window) {
 
@@ -523,4 +530,59 @@ $('.slider').slick({
 
 })(window);
 
+// ----수정------
+(function() {
 
+	const target = document.querySelector(".target");
+	const links = document.querySelectorAll(".menu-box a");
+	const colors = ["deepskyblue", "black", "darkblue"];
+  
+	function mouseenterFunc() {
+	  if (!this.parentNode.classList.contains("active")) {
+		for (let i = 0; i < links.length; i++) {
+		  if (links[i].parentNode.classList.contains("active")) {
+			links[i].parentNode.classList.remove("active");
+		  }
+		  links[i].style.opacity = "0.25";
+		}
+  
+		this.parentNode.classList.add("active");
+		this.style.opacity = "1";
+  
+		const width = this.getBoundingClientRect().width;
+		const height = this.getBoundingClientRect().height;
+		const left = this.getBoundingClientRect().left + window.pageXOffset;
+		const top = this.getBoundingClientRect().top + window.pageYOffset;
+		const color = colors[Math.floor(Math.random() * colors.length)];
+  
+		target.style.width = `${width}px`;
+		target.style.height = `${height}px`;
+		target.style.left = `${left}px`;
+		target.style.top = `${top}px`;
+		target.style.borderColor = color;
+		target.style.transform = "none";
+	  }
+	}
+  
+	for (let i = 0; i < links.length; i++) {
+	  links[i].addEventListener("click", (e) => e.preventDefault());
+	  links[i].addEventListener("mouseenter", mouseenterFunc);
+	}
+  
+	function resizeFunc() {
+	  const active = document.querySelector(".menu-box li.active");
+  
+	  if (active) {
+		const left = active.getBoundingClientRect().left + window.pageXOffset;
+		const top = active.getBoundingClientRect().top + window.pageYOffset;
+  
+		target.style.left = `${left}px`;
+		target.style.top = `${top}px`;
+	  }
+	}
+  
+	window.addEventListener("resize", resizeFunc);
+  
+  })();
+
+  // ----수정------
